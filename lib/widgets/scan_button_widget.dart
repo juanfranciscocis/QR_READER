010acc48b292795d52c35a223f37a09ca53efc1c
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-23 00:07:39 -0500

    Database starting

--- lib/widgets/scan_button_widget.dart
+++ lib/widgets/scan_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButtonWidget extends StatelessWidget{
   const ScanButtonWidget({Key? key}) : super(key: key);
@@ -6,7 +7,17 @@ class ScanButtonWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: (){
+      onPressed: () async {
+
+
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancel', false, ScanMode.QR); //https://pub.dev/packages/flutter_barcode_scanner
+        final barcodeScanRes = 'www.google.com'; //emulate the barcodeScanRes
+        print(barcodeScanRes);
+
+
+
+
+
       },
       elevation: 10,
       child: Icon(Icons.camera_alt),
